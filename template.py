c1b6e2704008226acaf26d25f4e60b33bb4a235d
Derrick Njobuenwu <donadviser@gmail.com>
2024-11-14 20:20:43 +0000

    add logging & custom exception

--- template.py
+++ template.py
@@ -18,8 +18,8 @@ list_of_files = [
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
     f"{project_name}/entity/artifact_entity.py",
-    f"{project_name}/exception/__init__.py",
-    f"{project_name}/logger/__init__.py",
+    #f"{project_name}/exception/__init__.py",
+    #f"{project_name}/logger/__init__.py",
     f"{project_name}/pipline/__init__.py",
     f"{project_name}/pipline/training_pipeline.py",
     f"{project_name}/pipline/prediction_pipeline.py",
@@ -46,7 +46,7 @@ list_of_files = [
 
     "references/.gitkeep",
     "notebooks/.gitkeep",
-    
+
     ".env.example"
 ]
 
